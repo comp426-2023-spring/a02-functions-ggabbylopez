f07643e4e4ea589c45b966d9f5ccd24404471e9a
ggabbylopez <gml0426@email.unc.edu>
2023-02-09 22:02:36 -0500

    Having a syntax error not sure what to do, will figure it ouT

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import moment from "moment-timezone";
+
+
 import minimist from 'minimist';
 import fetch from 'node-fetch';
+import moment from "moment-timezone";
 
 const args = minimist(process.argv.slice(2));
 const help_statement = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
@@ -15,8 +17,6 @@ const help_statement = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGIT
 if (args.h){
     console.log(help_statement);
     process.exit(0);
-} else {
-    process.exit(1);
 }
 
 
@@ -25,15 +25,21 @@ const timezone = moment.tz.guess();
 var latitude = args.n || -1 * args.s;
 var longitude = args.e || -1 * args.w;
 
-const url = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone);
-const data = await url.j;
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=precipitation_hours&temperature_unit=fahrenheit&timezone=' + timezone);
+const data = await response.j;
 
 if (args.j){
     console.log(data);
     process.log(0);
-} else {
-    process.log(1);
 }
 
 
 const days = args.d;
+
+if (days == 0){
+    console.log("today.");
+} else if (days > 1){
+    console.log("in" + days + "days.");
+} else {
+    console.log("tomorrow.");
+}
